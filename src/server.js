df55623ae799b8312f3f8b337d440cda9fd9eec7
Anika <anikaadventurer@gmail.com>
2026-03-12 15:29:49 +0000

    Fix MongoDB connection for Railway

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb://mongo:27017/todos");
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/todos");
 
 app.use("/", todoRoutes);
 
